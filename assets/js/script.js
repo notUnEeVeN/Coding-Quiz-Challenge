7a7668b79f6bb223d9875943ececccfc119574a9
notUnEeVeN <tybalt.mallet@gmail.com>
2023-07-03 14:14:55 -0700

    added all the quiz pages, first attempt at logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ let gameOver = false;
 
 document.getElementById("start-button").addEventListener("click", function () {
     var homepage = document.getElementsByClassName("homepage");
-    var quizpage = document.getElementsByClassName("quizpage");
+    var quizpage = document.getElementsByClassName("quizpage1");
     for (let i = 0; i < homepage.length; i++) {
         homepage[i].style.display = "none";
     };
@@ -15,7 +15,7 @@ document.getElementById("start-button").addEventListener("click", function () {
         quizpage[i].style.display = "block";
     };
 
-    timer = 75;
+    timer = 5;
     document.getElementById("timer").textContent = `Time left: ${timer}` ;
     clearInterval(timerInterval);
     timerInterval = setInterval(function() {
@@ -25,7 +25,26 @@ document.getElementById("start-button").addEventListener("click", function () {
         if(timer <= 0) {
             clearInterval(timerInterval);
             gameOver = true;
+            for (let i = 0; i < quizpage.length; i++) {
+                quizpage[i].style.display = "none";
+            }
         }
-    }, 1000)
+    }, 1000);
+    for (let i = 0; i < quizpage.length; i++) {
+        quizpage[i].addEventListener('click', function(event) {
+            if(event.target.tagName === 'BUTTON') {
+                for (let i = 0; i < quizpage.length; i++) {
+                    quizpage[i].style.display = "none";
+                };
 
+                
+                if(event.target.getAttribute('data-correct') === 'true') {
+
+                } else {
+
+                }
+            }
+        });
+    };
+   
 });
